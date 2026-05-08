6fc348ed4fd918ac06751922f7afcd8722d78336
suvetharani <suvetharanic@gmail.com>
2026-05-08 16:08:12 +0530

    Fix deployment settings

--- lms-backend/server.js
+++ lms-backend/server.js
@@ -21,7 +21,13 @@ const pushRoutes = require("./routes/pushRoutes");
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://learner-s-hub-6x42.vercel.app/"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
@@ -47,8 +53,12 @@ app.use("/uploads", express.static("uploads"));
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://learner-s-hub-6x42.vercel.app/"
+    ],
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
@@ -74,6 +84,8 @@ io.to(receiverSocket).emit("receiveMessage", {
   });
 });
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
